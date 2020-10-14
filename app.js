438a824d5832a3c607a22a08f634a3471020b275
Ryan C <ryan.a.chowdhry@gmail.com>
2020-10-14 15:14:29 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+class UI {
+    constructor() {
+
+    }
+}
+function eventListeners(){
+const budgetForm = document.getElementById('budget-form')
+const expenseForm = document.getElementById('expense-form'); 
+const expenseList = document.getElementById('expense-list'); 
+}
+
+// UI Class - New Instance 
+const ui = new UI()
+document.addEventListener('DOMContentLoaded', function(){
+    eventListeners(); 
+})
