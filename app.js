59cc66849c32f579e77a8ca8da6210562a87d4d6
Ryan C <ryan.a.chowdhry@gmail.com>
2020-10-16 10:13:46 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 class UI {
+    //? b = BUDGET 
+    //? e = EXPENSE  
+    //? a = AMOUNT 
+    
     constructor() {
-      this.budgetFeedback = document.querySelector(".budget-feedback");
-      this.expenseFeedback = document.querySelector(".expense-feedback");
-      this.budgetForm = document.getElementById("budget-form");
-      this.budgetInput = document.getElementById("budget-input");
-      this.budgetAmount = document.getElementById("budget-amount");
-      this.expenseAmount = document.getElementById("expense-amount");
+      this.bFeedback = document.querySelector(".budget-feedback");
+      this.eFeedback = document.querySelector(".expense-feedback");
+      this.bForm = document.getElementById("budget-form");
+      this.bInput = document.getElementById("budget-input");
+      this.bAmount = document.getElementById("budget-amount");
+      this.eAmount = document.getElementById("expense-amount");
       this.balance = document.getElementById("balance");
-      this.balanceAmount = document.getElementById("balance-amount");
-      this.expenseForm = document.getElementById("expense-form");
-      this.expenseInput = document.getElementById("expense-input");
-      this.amountInput = document.getElementById("amount-input");
-      this.expenseList = document.getElementById("expense-list");
+      this.bAmount = document.getElementById("balance-amount");
+      this.eForm = document.getElementById("expense-form");
+      this.eInput = document.getElementById("expense-input");
+      this.aInput = document.getElementById("amount-input");
+      this.eList = document.getElementById("expense-list");
       this.itemList = [];
       this.itemID = 0;
     }
@@ -19,22 +23,46 @@ class UI {
 submitBudForm() { 
 const value = this.budgetInput.value; 
 if(value ==='' || value<0){
-this.budgetFeedback.classList.add("showItem");
-this.budgetFeedback.innerHTML = `<p> ⛔ NOTE: Do not insert a negative/empty property :)</p>`; 
-this.budgetAmount.textContent = value; 
-this.budgetInput.value = ''; 
+this.bFeedback.classList.add("showItem");
+this.bFeedback.innerHTML = `<p> 
+⛔ NOTE: Do not insert a negative/empty property :)
+</p>`; 
+this.bAmount.textContent = value; 
+this.bInput.value = ''; 
 this.sBalance(); 
 }
     }
 // sBalance part 
 sBalance(){
 const expense = this.tExpense(); 
-const total = parseInt = parseInt(this.budgetAmount.textContent) - expense; 
-this.balanceAmount.textContent = total; 
+const total = parseInt = parseInt(this.bAmount.textContent) - expense; 
+this.bAmount.textContent = total; 
 if(total < 0){
-    this.balance.classList.remove('showGreen', '')
+    this.balance.classList.remove('showGreen', 'showBlack');
+    this.balance.classList.add('showRed');
 }
-} 
+else if(total > 0){
+    this.balance.classList.remove('showRed', 'showBlack');
+    this.balance.classList.add('showGreen');
+}
+else if(total === 0){
+    this.balance.classList.remove('showRed', 'showGreen');
+    this.balance.classList.add('showBlack');
+}
+}
+
+// SUBMIT EXPENSE FORM 
+submitExpenseForm() { 
+    const eValue = this.expenseInput.value; 
+    const aValue = this.accountInput.value; 
+    if(eValue === '' || aValue === '0' || aValue < 0){
+        this.eFeedback.classList.add("sItem");
+        this.eFeedback.innerHTML = `<p> 
+        ⛔ NOTE: Do not insert a negative/empty property :) 
+        </p>`;  
+        const self = this; 
+    }
+}
 
 // tExpense part
 tExpense(){
@@ -45,26 +73,27 @@ tExpense(){
 
 
 function eventListeners(){
-const budgetForm = document.getElementById('budget-form')
-const expenseForm = document.getElementById('expense-form'); 
-const expenseList = document.getElementById('expense-list'); 
+const bForm = document.getElementById('budget-form')
+const eForm = document.getElementById('expense-form'); 
+const eList = document.getElementById('expense-list'); 
 }
 
 // UI Class - New Instance 
 const ui = new UI()
 
 // budget form submit
-budgetForm.addEventListener('submit', function(event){
+bForm.addEventListener('submit', function(event){
 
 }); 
 
 // expense form submit 
-expenseForm.addEventListener('submit', function(event){
+eForm.addEventListener('submit', function(event){
     event.preventDefault(); 
+ui.submitExpenseForm();
 }); 
 
 // expense click 
-expenseList.addEventListener('click!', function() {});
+eList.addEventListener('click!', function() {});
 document.addEventListener('DOMContentLoaded', function(){
  
 })
